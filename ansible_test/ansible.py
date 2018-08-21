739938f81b57044ae8ea0cf7051cb54eb9bb5348
Guillaume 'Llew' DELPIERRE <github@llew.me>
2018-08-21 09:41:58 +0200

    chore(ansible.py): generate inventory and playbook file using j2

--- ansible_test/ansible.py
+++ ansible_test/ansible.py
@@ -1,20 +1,51 @@
+"""
+Handles ansible files
+"""
 from os import path
+
 from jinja2 import Template
 from pkg_resources import resource_string
+
 from config import ANSIBLE_TESTDIR
 
-def generate_inventory():
-    with open(path.join(ANSIBLE_TESTDIR, "inventory.yml"), "wb") as f:
-        f.write(resource_string("ansible_test", "resources/inventory.yml"))
+
+def generate_inventory(options):
+    """
+    Generate inventory file based on jinja2 template
+    """
+    rendered_inventory_file = Template(
+        resource_string('ansible_test',
+                        'templates/inventory.yml.j2'
+                       ).render(options)
+    )
+    with open(path.join(ANSIBLE_TESTDIR,
+                        'inventory.yml'), 'wb') as inventory_file:
+        inventory_file.write(rendered_inventory_file)
+
 
 def generate_playbook(options):
-    with open(path.join(ANSIBLE_TESTDIR, "playbook.yml"), "wb") as f:
-        f.write(resource_string("ansible_test", "resources/playbook.yml"))
+    """
+    Generate playbook file based on jinja2 template
+    """
+    rendered_playbook_file = Template(
+        resource_string('ansible_test',
+                        'templates/playbook.yml.j2'
+                       ).render(options)
+    )
+    with open(path.join(ANSIBLE_TESTDIR,
+                        'playbook.yml'), 'wb') as playbook_file:
+        playbook_file.write(rendered_playbook_file)
+
 
 def execute_playbook():
+    """
+    """
     # TODO
     print("TODO")
 
+
 def syntax_check():
+    """
+    """
     # TODO
     print("TODO")
